af249fdbb28eb171248f9b7fc5943c825567e9ea
Deeanj1 <deeanj2003@gmail.com>
2025-05-07 20:30:13 +0100

    update

--- script.js
+++ script.js
@@ -13,13 +13,13 @@
 
 
 
-//   primitive data types
-//  primitive data types
+// //  primitive data types
+// //  primitive data types
  
 // // string '' ""
 // let kezaya = "today is kezaya's birthday"
 // console.log(kezaya);
-// number 
+// // number 
 
 // let mynum = 63
 // console.log(mynum);
@@ -172,7 +172,7 @@
 // console.log(k);
 
 // Ternary Operators //Ternary Operators //Ternary Operators
-// Ternary Operators //Ternary Operators //Ternary Operators
+// // Ternary Operators //Ternary Operators //Ternary Operators
 
 // const age = 19;
 
@@ -237,6 +237,11 @@
 // let ans = valueOne + valueTwo;
 // alert(ans);
 
+
+
+//ACTIVITIES //ACTIVITIES //ACTIVITIES
+//ACTIVITIES //ACTIVITIES //ACTIVITIES
+
 //a program that checks if a number is positive or negative
 
 // let number = parseInt(prompt("Enter a number"));
@@ -247,12 +252,110 @@
 //     alert(`${number} is negative`);
 // }
 
+// cont.d // cont.d // cont.d // cont.d
+// cont.d // cont.d // cont.d // cont.d
+
+
+// const usernumber = parseInt(prompt("Enter a number"));
+// if (usernumber > 0) {
+//     alert(`${usernumber} is positive`);
+// }
+// else if (usernumber === 0) {
+//     alert(`${usernumber} is invalid`);
+// }
+// else if (isNaN(usernumber)) {
+//     alert(`This is not a number`);
+// }
+// else{
+//     alert(`${usernumber} is negative`);
+// }
+
+
 //a program that checks if a number is even or odd
 
-let number = parseInt(prompt("Enter a number"));
-if (number % 2 === 0) {
-    alert(`${number} is an even number  `);
+// let number = parseInt(prompt("Enter a number"));
+// if (number % 2 === 0) {
+//     alert(`${number} is an even number  `);
+// }
+// else {
+//     alert(`${number} is an odd number`);
+// }
+
+
+// a program that determines the greatest of two numbers
+
+// let num1 = parseInt(prompt("Enter first number"));
+// let num2 = parseInt(prompt("Enter second number"));
+
+// if (num1 > num2) {
+//     alert(`${num1} is greater than ${num2}`);
+// }
+// else if (num1 < num2) {
+//     alert(`${num2} is greater than ${num1}`);
+// }
+// else if (num1 === num2) {
+//     alert(`${num1} is equal to ${num2}`);
+// }
+// else if (isNaN(num1) || isNaN(num2)) {
+//     alert(`This is not a number you fucking cunt`);
+// }
+// else {
+//     alert(`Invalid input`);
+// }
+
+//write a program that transforms a numerical grade to a letter grade
+
+//  let grade = parseInt(prompt("Enter your grade"));
+// if (grade >= 70 && grade <= 100) {
+//     alert(`Your grade is A`);       
+// }
+// else if (grade >= 60 && grade <= 69) {
+//     alert(`Your grade is B`); 
+// }
+// else if (grade >= 50 && grade <= 59) {
+//     alert(`Your grade is C`); 
+// }
+// else if (grade >= 40 && grade <= 49) {
+//     alert(`Your grade is D`); 
+// }
+// else if (grade >= 0 && grade <= 39) {
+//     alert(`Your grade is F`); 
+// }
+// else if (grade < 0 || grade > 100) {
+//     alert(`This is not a valid grade`);
+// }
+// else if (isNaN(grade)) {
+//     alert(`This is not a number you fucking cunt`);
+// }
+// else {
+//     alert(`Invalid input`);
+// }
+
+
+//write a program that calculates the body mass index (BMI) and categorizes it. The formula for BMI is: weight / (height * height). The categories are as follows: underweight (below 18.5), normal weight (18.5t to 24.9), overweight (25 to 29.9), and obesity (30 or greater).
+
+let weight = parseFloat(prompt("Enter your weight in kg"));
+let height = parseFloat(prompt("Enter your height in meters"));
+
+let bmi = weight / (height * height);
+alert(`Your BMI is ${bmi}`);
+
+if (bmi < 18.5) { 
+    alert(`You are underweight`);
+}
+else if (bmi >= 18.5 && bmi <= 24.9) {
+    alert(`You are normal weight`);
+}
+else if (bmi >= 25 && bmi <= 29.9) {
+    alert(`You are overweight`);
+}
+else if (bmi >= 30) {
+    alert(`You are obese`);
+}
+else if (isNaN(bmi)) {
+    alert(`This is not a number you fucking cunt`);
 }
 else {
-    alert(`${number} is an odd number`);
+    alert(`Invalid input`);
 }
+
